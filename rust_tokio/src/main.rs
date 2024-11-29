4f6c4bae5584332afd5b77447da18e960335cec7
Omid Rad <omidmr@gmail.com>
2024-11-29 14:11:38 +0100

    Improve rust resource usage

--- rust_tokio/src/main.rs
+++ rust_tokio/src/main.rs
@@ -1,13 +1,15 @@
 use std::env;
 use tokio::time::{sleep, Duration};
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let num_tasks = args[1].parse::<i32>().unwrap();
-    let mut tasks = Vec::new();
-    for _ in 0..num_tasks {
-        tasks.push(sleep(Duration::from_secs(10)));
+    let tasks = (0..num_tasks)
+        .map(|_| sleep(Duration::from_secs(10)))
+        .collect::<Vec<_>>();
+
+    for task in tasks {
+        task.await;
     }
-    futures::future::join_all(tasks).await;
 }
