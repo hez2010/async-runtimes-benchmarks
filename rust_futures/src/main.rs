f81ab241836dd480bfd4033d664d8904cb18d9f1
Steven He <hez2010@outlook.com>
2024-12-01 15:40:43 +0900

    Enable rt-multi-thread

--- rust_futures/src/main.rs
+++ rust_futures/src/main.rs
@@ -1,10 +1,10 @@
 use std::env;
 use std::time::Duration;
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    let num_tasks = args[1].parse::<usize>().unwrap();
+    let num_tasks = env::args().skip(1).next().unwrap().parse().unwrap();
+
     futures::future::join_all((0..num_tasks).map(|_| tokio::time::sleep(Duration::from_secs(10))))
         .await;
 }
